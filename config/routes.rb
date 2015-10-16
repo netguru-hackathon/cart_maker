9891e02bb861cf6eecbfd38ff3a87bbe633d7f28
Maciej Kalisz <mdkalish4git@gmail.com>
2015-10-16 18:16:44 +0200

    Update root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root 'carts#index'
   resources :cart_products
   resources :carts
   resources :products
   resources :cities
   devise_for :users, controllers: { registrations: 'user/registrations' }
-  root to: 'visitors#index'
 
   namespace :api do
     post :update, as: "add_product", to: "carts_controller#update"
