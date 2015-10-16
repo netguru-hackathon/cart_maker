81d772bec5b9c3af3e1a02b103d3a2a3c9362b87
Maciej Kalisz <mdkalish4git@gmail.com>
2015-10-16 18:33:05 +0200

    Seed admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@ cities.each do |c|
     region_name: c
   )
 end
+
+unless user = User.find_by_email('admin@admin.admin')
+  User.create!(
+    email: 'admin@admin.admin',
+    password: 'asdasdasd',
+    city_id: City.first.id)
+else
+  user.update(password: 'asdasdasd',
+              city_id: City.first.id)
+end
+puts "Log in with: admin@admin.admin, password: 'asdasdasd'"
