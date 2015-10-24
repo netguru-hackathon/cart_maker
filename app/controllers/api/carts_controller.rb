46625707e1f5841ab3aa8dc346822694ae68d3d7
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2015-10-24 18:22:47 +0200

    fix api controller for adding new product from slack

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -5,14 +5,15 @@ module Api
     skip_before_action :verify_authenticity_token
 
     def update
-      @cart = City.where(region_name: params[:region_name]).first.carts.first
-      pip_id = params[:product_link].slice(1..-2).split('/').last
+      @cart = City.where(region_name: params[:region_name]).first.carts.first_or_create
+      correct_uri = params[:product_link].slice(1..-2)
+      pip_id = correct_uri.split('/').last
       @product = Product.where(pip_id: pip_id).first || Product.new(pip_id: pip_id)
 
       if @product.name.blank?
-        uri = URI.parse(params[:product_link])
+        uri = URI.parse(correct_uri)
         response = Net::HTTP.get_response(uri)
-        @product.name = Net::HTTP.get_print(uri).scan(/(?<=itemprop=\"name\">)(.*)(?=<)/)
+        @product.name = response.body.scan(/(?<=itemprop=\"name\">)(.*)(?=<)/).first.first || ""
         @product.save
       end
 
@@ -21,6 +22,7 @@ module Api
       @cart_product.product_id = @product.id
       @cart_product.quantity = params[:product_amount]
       @cart_product.save
+      render json: @cart_product
     end
 
     private
