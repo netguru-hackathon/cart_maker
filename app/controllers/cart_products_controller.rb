d9b82e9e933c5cedeadc9a16c8b6eacbabd99532
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2015-10-25 13:30:00 +0100

    a lot of work on designs

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,28 +1,21 @@
 class CartProductsController < ApplicationController
   before_action :set_cart_product, only: [:show, :edit, :update, :destroy]
 
-  # GET /cart_products
-  # GET /cart_products.json
   def index
-    @cart_products = CartProduct.all
+    redirect_to root_path, notice: 'Wrong'
   end
 
-  # GET /cart_products/1
-  # GET /cart_products/1.json
   def show
+    redirect_to cart_path(@cart_product.cart_id)
   end
 
-  # GET /cart_products/new
   def new
     @cart_product = CartProduct.new
   end
 
-  # GET /cart_products/1/edit
   def edit
   end
 
-  # POST /cart_products
-  # POST /cart_products.json
   def create
     @cart_product = CartProduct.new(cart_product_params)
 
@@ -37,8 +30,6 @@ class CartProductsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /cart_products/1
-  # PATCH/PUT /cart_products/1.json
   def update
     respond_to do |format|
       if @cart_product.update(cart_product_params)
@@ -51,8 +42,6 @@ class CartProductsController < ApplicationController
     end
   end
 
-  # DELETE /cart_products/1
-  # DELETE /cart_products/1.json
   def destroy
     @cart_product.destroy
     respond_to do |format|
@@ -62,13 +51,12 @@ class CartProductsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cart_product
-      @cart_product = CartProduct.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def cart_product_params
-      params.require(:cart_product).permit(:cart_id, :quantity)
-    end
+  def set_cart_product
+    @cart_product = CartProduct.find(params[:id])
+  end
+
+  def cart_product_params
+    params.require(:cart_product).permit(:cart_id, :quantity)
+  end
 end
